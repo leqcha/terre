b408e51d62ddf9b10725ae3d28573d5d61b162ae
charlotte <charlotte.lequesne@hotmail.fr>
2023-07-18 17:39:08 +0200

    exercices

--- terre15.js
+++ terre15.js
@@ -1,11 +1,3 @@
-// Créez un programme qui célèbre votre victoire.
-
-// Exemples d’utilisation :
-// $> ruby exo.rb
-// J’ai terminé l’Épreuve de la Terre et c’était [].
-// $>
-
-// Note : [] est à remplacer par un adjectif de votre choix (facile ?)
 let happyEarth = "";
 for (let i = 0; i < 19; i++){
     happyEarth += "\u{1F30D}";
